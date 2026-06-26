67aa2c066150e63b3119dc44bc3f909eec4d118e
remmy-web <137243748+remmy-web@users.noreply.github.com>
2026-06-26 10:45:12 +0300

    added white noise to the middleware

--- Emilly_data_collector/Emilly_data_collector/settings.py
+++ Emilly_data_collector/Emilly_data_collector/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
