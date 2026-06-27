3d709c8831336250f39c389ea51a74159367e103
remmy-web <137243748+remmy-web@users.noreply.github.com>
2026-06-27 06:47:31 +0300

    added the download data endpoints and logic

--- Emilly_data_collector/api/urls.py
+++ Emilly_data_collector/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import OnionRetailSurveyCreateView
+from . import views
 
 urlpatterns = [
-    path('add/',OnionRetailSurveyCreateView.as_view(),name="add_survey_data")
+    path('add/',views.OnionRetailSurveyCreateView.as_view(),name="add_survey_data"),
+    path('get/',views.view_survey_data,name="get_survey_data"),
+    path('download_data/', views.download_survey_data,name="download_survey_data")
 ]
